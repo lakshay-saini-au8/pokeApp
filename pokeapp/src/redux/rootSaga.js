04f618313dc2a05d82df4c4520930cf157bcccf5
Lakshay saini <lakshaysaini2013@gmail.com>
2020-10-23 02:34:22 +0530

    final

--- pokeapp/src/redux/rootSaga.js
+++
@@ -1,9 +0,0 @@
-import { all } from "redux-saga/effects";
-import watchFetchAllPokemon from "./sagas/getAllPokemonSaga";
-import watchFetchSinglePokemon from "./sagas/getSinglePokemonSaga";
-
-function* rootSaga() {
-  yield all([watchFetchAllPokemon(), watchFetchSinglePokemon()]);
-}
-
-export default rootSaga;
